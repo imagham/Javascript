1ea1678d05bdf5613d73524246b0d5985bb52854
imagham <151702624+imagham@users.noreply.github.com>
2024-05-09 11:53:41 +0700

    latihan 05

---
+++ latihan js/latihan js 5/index5.js
@@ -0,0 +1,61 @@
+// soal 1
+let permen=30
+let saya =30-10
+console.log(saya);
+var permenbaru=20*2
+console.log(permenbaru);
+var yganapunya=40**2
+console.log(yganapunya);
+// soal 2
+var kucing1='ali'+'hasan' 
+console.log(kucing1);
+// soal 3
+var kucing='12';
+var nyamuk='11';
+var angsa=10;
+console.log(+kucing+nyamuk);
+console.log(+nyamuk+angsa);
+// soal 4
+var buku='buku fiksi'
+var buku='buku non-fiksi'
+var buku='pena'
+var harga1=5000
+var harga2=7000
+var harga3=2000
+var jumlah1=5
+var jumlah2=3
+var jumlah3=2
+var totalfiksi=harga1*jumlah1
+var totalnonfiksi=harga2*jumlah2
+var pena=harga3*jumlah3
+console.log(totalfiksi);
+console.log(totalnonfiksi);
+console.log(pena);
+console.log(totalfiksi+totalnonfiksi+pena);
+// soal 5
+var penjumlahan1=5
+var penjumlahan2=3
+var penjumlahan3=2
+console.log(penjumlahan1+penjumlahan2*penjumlahan3);
+// soal 6
+var kresek=4
+var kotak=6
+var ember=5+(kresek=kotak)
+console.log(ember);
+// soal 7
+var x=5
+var y=10
+var z=15
+console.log(x+y+z);
+// soal 8
+var product=4*2
+var hasil=product*2
+var hassil=hasil*2
+console.log(hassil);
+// soal 9
+var x=5
+x++
+console.log(x);
+var y=8
+y--
+console.log(y);
